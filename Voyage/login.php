9e727db8cf9f2920e2532f098a84a72d93ff4d42
Angela Sanchez <60831893+angelllaamariee@users.noreply.github.com>
2021-10-07 17:12:21 -0400

    Update login.php

--- Voyage/login.php
+++ Voyage/login.php
@@ -1,19 +1,20 @@
 <?php
 	$userName = $_POST['userUid'];
 	$pwd = $_POST['userPassword'];
-	$conn = new mysqli('localhost', 'root','','users');
+	$conn = new mysqli('localhost', 'root','','users');//
+//Conn variable is what connects to the database
 	if($conn->connect_error){
-		die('Connection Failed: '.$conn->connect_error);
+		die('Connection Failed: '.$conn->connect_error);//shows if an error happened and what error it was
 	}
 	else{
-		$stmt = $conn->prepare("SELECT * FROM users WHERE userUid = ?");
+		$stmt = $conn->prepare("SELECT * FROM users WHERE userUid = ?"); //preparing my select table from the users to access userid
 		$stmt -> bind_param("s", $userName);
 		$stmt -> execute();
-		$stmt_result = $stmt->get_result();
+		$stmt_result = $stmt->get_result();//The result to check if the username already exists in the database
 		if($stmt_result->num_rows>0){
-			$data = $stmt_result->fetch_assoc();
-			if($data['userPassword'] === $pwd){
-				echo "<center><video controls autoplay> <source src='logo.mp4' type='video/mp4'></video></center>";
+			$data = $stmt_result->fetch_assoc();//if num of rows is greater than 0 
+			if($data['userPassword'] === $pwd){ //check if the pwd from database is matching the password user input
+				echo "<center><video controls autoplay> <source src='logo.mp4' type='video/mp4'></video></center>"; //successfully entered
 			}
 		
 		
