ef5f8a03efbc7fa6cbef0b4851c3b0f900bed92b
Angela Sanchez <60831893+angelllaamariee@users.noreply.github.com>
2021-10-20 16:05:49 -0400

    Update voyage.php

--- Voyage/voyage.php
+++ Voyage/voyage.php
@@ -1,35 +1,39 @@
-<!DOCTYPE html> 
-<html>
-	<head> 
-		<meta charset ="utf-8"> 
-		<title>The Voyage News</title>
-	</head>
-	<body>
-		<link rel = "stylesheet" href="style.css">
-		<div class="begin">
-		<div class="size-box">
-		<div class = "box-botton">
-		<div id = "color-botton"></div>
-		<button type= "button" class="switch-botton" onclick = "login()">Log In</botton>
-		<button type= "button" class="switch-botton" onclick = "register()">Register</botton>
-		</div>
-		<form id = "login" class = "input-text" method="POST" action="login.php"> //given class name so I am able to change each individual feature using CSS code 
-			<input type = "text" class = "inputting-text" placeholder = "User ID" name="userUid" required>
-			<input type = "password" class = "inputting-text" placeholder = "Enter Password" name="userPassword" required>
-			<input type = "checkbox" class = "check-box"><span>Remember Password</span>
-			<button type = "submit" name="submit "class = "submit-botton">Log In</button>
-		</form>
-		<form id = "register" class = "input-text" method="post" action="signup.php">
-		<input type = "text" class = "inputting-text" placeholder = "Name" name="userName" required>
-		<input type = "text" class = "inputting-text" placeholder = "User ID" name="userUid" required>
-		<input type = "email" class = "inputting-text" placeholder = "Email ID" name="userEmail" required>
-		<input type = "password" class = "inputting-text" placeholder = "Enter Password" name="userPassword" required>
-		<input type ="password" class="inputting-text"placeholder="Confirm Password" name="pwdRepeat" required>
-		<input type = "checkbox" class = "check-box"><span>I Agree Voyage News is my #1 News Outlet</span>
-		<button type = "submit" name = "submit" class = "submit-botton">Register</button>
-		</form>
-		</div>
-		</div>
-		<script src="script.js"></script>
-	</body>
+html>
+<head>
+<title>The Voyage News</title>
+<link rel = "stylesheet" href="style.css">
+</head>
+<body>
+<div class="begin">
+<div class="size-box">
+<div class = "box-botton">
+<div id = "color-botton"></div>
+<button type= "button" class="switch-botton" onclick = "login()">Log In</botton>
+<button type= "button" class="switch-botton" onclick = "register()">Register</botton>
+</div>
+<form id = "login" class = "input-text">
+<input type = "text" class = "inputting-text" placeholder = "User ID" required>
+<input type = "password" class = "inputting-text" placeholder = "Enter Password" required>
+<input type = "checkbox" class = "check-box"><span>Remember Password</span>
+</br>
+<a href="forgotindex.html" class="info-btn">Forgot Password</a>
+<button type = "submit" class = "submit-botton">Log In</button>
+</form>
+
+<form id = "register" class = "input-text">
+<input type = "text" class = "inputting-text" placeholder = "Name" required>
+<input type = "text" class = "inputting-text" placeholder = "User ID" required>
+<input type = "email" class = "inputting-text" placeholder = "Email ID" required>
+<input type = "password" class = "inputting-text" placeholder = "Enter Password" required>
+<input type ="password confirmation" class="inputting-text"placeholder="Password Confirmation" required>
+<input type = "checkbox" class = "check-box"><span>I Agree Voyage News is my #1 News Outlet</span>
+<button type = "submit" class = "submit-botton">Register</button>
+</form>
+
+</div>
+
+</div>
+
+<script src="script.js"></script>
+</body>
 </html>
