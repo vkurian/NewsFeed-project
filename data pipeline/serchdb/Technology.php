ecaae9bed9c2f589502688c1992dcc9ff6a711c3
vkurian <73532095+vkurian@users.noreply.github.com>
2021-12-09 16:03:55 -0500

    Create Technology.php

---
+++ data pipeline/serchdb/Technology.php
@@ -0,0 +1,41 @@
+<?php
+include 'connect.php'
+?>
+<!doctype html>
+<html>
+<title> PHP MYSQL - Search</title>
+<head>
+</head>
+
+<body>
+<form action="result.php" method="POST">
+<h3>Search Database</h3>
+	<p>Search</p>
+	<input type="text" name="name" size="100"></td>
+	<input type="submit" name="submit">
+</form>
+</body>
+<?php
+$sql="SELECT * FROM technology";
+$result= mysqli_query($conn,$sql);
+$queryResults= mysqli_num_rows($result);
+			
+	if($queryResults){
+		while($row=mysqli_fetch_assoc($result))
+			{
+				?>
+					
+					
+				<div>
+					<img src="<?php echo $row['urlToImage']; ?>" width="500" height="500">
+					<h2>Title:<?php echo $row['title']?></h2>
+					<h2>author: <?php echo $row['author']?></h2>
+					<h2> url:<?php echo"<a href='$row[url]'>{$row['url']}</a><br/>"?>
+					<h2>description: <?php echo $row['description']?></h2>
+					<h2>published: <?php echo $row['publishedAt']?></h2>
+					</div>
+			<?php		
+
+			}
+			}
+		?>
