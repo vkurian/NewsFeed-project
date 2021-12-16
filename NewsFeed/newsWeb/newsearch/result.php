caefc6d5b58a3a24a64f83c01f0f4ccf5b144f16
Angela Sanchez <60831893+angelllaamariee@users.noreply.github.com>
2021-12-16 02:36:07 -0500

    Update result.php

--- NewsFeed/newsWeb/newsearch/result.php
+++ NewsFeed/newsWeb/newsearch/result.php
@@ -1,5 +1,35 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "","test");
+?>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=.0">
+<title> PHP MYSQL - Search</title>
+<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+</head>
+
+<body>
+<section>
+      <header>
+        <h2><a href="#" class="logo">Voyage News Outlet</a></h2>
+        <div class="navigation">
+        </div>
+      </header>
+<form action="result.php" method="POST">
+<center><h3 class="logo" >Search General</h3></center>
+<center><table>
+<tr>
+	<td><input class = "input" type="text" name="name"></td>
+	<td><input class = "click" type="submit" name="submit"></td>
+</tr>
+</table></center>
+</form>
+</body>
+<?php
+$conn = mysqli_connect("localhost", "root", "","test");
 if($_REQUEST['submit']){
 $name = $_POST['name'];
 
@@ -32,15 +62,14 @@ if(empty($name)){
 	if($row = mysqli_num_rows($result) > 0){
 		while($row = mysqli_fetch_assoc($result)){
 			?>
-					<div>
-						<img src="<?php echo $row['urlToImage']; ?>" width="500" height="500">
-						<h2>Title:<?php echo $row['title']?></h2>
-						<h2>author: <?php echo $row['author']?></h2>
-						<h2> url:<?php echo"<a href='$row[url]'>{$row['url']}</a><br/>"?>
-						<h2>description: <?php echo $row['description']?></h2>
-						<h2>publishedAt: <?php echo $row['publishedAt']?></h2>
-					
-					</div>";
+					<div class= "content">
+					<img class = "image-page" src="<?php echo $row['urlToImage']; ?>" width="500" height="500">
+					<h2 class = "logo">Title:<?php echo $row['title']?></h2>
+					<h4 class = "logo">author: <?php echo $row['author']?></h4>
+					<h4 class = "logo_link"> url:<?php echo"<a href='$row[url]'>{$row['url']}</a><br/>"?>
+					<h4 class = "logo">description: <?php echo $row['description']?></h4>
+					<h4 class = "logo">published: <?php echo $row['publishedAt']?></h4>
+					</div>
 					<?php
 	}
 }else{
